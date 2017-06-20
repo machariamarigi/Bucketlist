3b9f84f55f722f392a6cff876cd50855f7bafce2
machariamarigi <machariamarigi@gmail.com>
2017-06-21 01:42:47 +0300

    Add user sign up method

--- app/tools.py
+++ app/tools.py
@@ -8,6 +8,18 @@ class Auth(object):
         This class should enable users to be able to sign up and sign into the
         app
     """
+    def __init__(self):
+        """ Auth Constructor"""
+        self.users = []
+
+    def signup(self, email, password):
+        user = {
+            'id': id + 1,
+            'email': email,
+            'password': password
+        }
+
+        self.users.append(user)
 
 
 class Storage(object):
