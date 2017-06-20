39db9271d317e2b0aaa7921086db58bb4c2d7ea5
machariamarigi <machariamarigi@gmail.com>
2017-06-20 11:41:34 +0300

    Set up testing environment, write first failing for the application's homepage

---
+++ tests/test_app_run.py
@@ -0,0 +1,20 @@
+""" Module to test the running the application """
+
+from unittest import TestCase
+
+from ..app import create_app
+
+
+class TestAppRun():
+    """Test the running of the application"""
+    def create_app(self):
+        """Create test instance of the application"""
+        config_name = 'testing'
+        app = create_app(config_name)
+        return app
+
+    def test_index(self):
+        """Test the loading of the homepage"""
+        app = self.create_app()
+        tester = app.get('/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
