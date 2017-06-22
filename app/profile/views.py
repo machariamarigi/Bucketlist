25f4a9f4f668cb0e92fdbc3b228dff426633f336
machariamarigi <machariamarigi@gmail.com>
2017-06-22 21:41:38 +0300

    Method for a user to create a bucketlist

--- app/profile/views.py
+++ app/profile/views.py
@@ -3,10 +3,18 @@
 from flask import render_template, session
 
 from . import profile
+# from .forms import BucketlistForm
+from ..models import BucketList
 
 
 @profile.route('/profile')
 def profilepage():
     """Render the homepage template on the / route"""
+
     user = session['username']
     return render_template('profile/profile.html', user=user)
+
+
+# @profile.route('/create_bucketlist', methods=['GET', 'POST'])
+# def create_bucketlist():
+    
